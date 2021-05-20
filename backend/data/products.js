ea95b05c257e4093b223a4e3fd5a777ddf2952ec
Sam Krol <samkrol97@gmail.com>
2021-05-20 12:48:24 +0100

    MERN Ecommerce Art Website

---
+++ backend/data/products.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    name: "",
+    imageUrl: "",
+    description: "",
+    price: 0,
+  },
+  {
+    name: "",
+    imageUrl: "",
+    description: "",
+    price: 0,
+  },
+  {
+    name: "",
+    imageUrl: "",
+    description: "",
+    price: 0,
+  },
+  {
+    name: "",
+    imageUrl: "",
+    description: "",
+    price: 0,
+  },
+  {
+    name: "",
+    imageUrl: "",
+    description: "",
+    price: 0,
+  },
+];
+
+module.exports = products;
